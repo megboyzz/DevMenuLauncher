b43b9d7464c0b88ef56a6c154df967662ae53ffe
Michael Kits <55844847+SeanGTX@users.noreply.github.com>
2022-10-22 19:13:03 +0300

    вывод корректного IP и обработка текущего состояния подключения. Поиск приложений с DevMenu

--- app/src/main/java/ru/megboyzz/devmenu/launcher/MainActivity.kt
+++ app/src/main/java/ru/megboyzz/devmenu/launcher/MainActivity.kt
@@ -1,6 +1,12 @@
 package ru.megboyzz.devmenu.launcher
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -11,24 +17,51 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.megboyzz.devmenu.launcher.ui.theme.MainBlue
 import ru.megboyzz.devmenu.launcher.ui.theme.MainWhite
+import java.net.NetworkInterface
+import java.net.SocketException
+import kotlin.concurrent.thread
+
 
 class MainActivity : ComponentActivity() {
+
+    private val devMenuPackageName = "com.devmenu.server.AppService"
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val pm = packageManager
+        val packages = pm.getInstalledPackages(PackageManager.GET_META_DATA or PackageManager.GET_SERVICES)
+        val devMenuAppsList = mutableListOf<PackageInfo>()
+
+        //Шерстим приложуху
+        thread {
+            for (packageInfo in packages) {
+                val services = packageInfo.services
+                if (services != null)
+                    for (service in services)
+                        if(service.name == devMenuPackageName) devMenuAppsList += packageInfo
+
+            }
+        }
+
         setContent {
 
             Scaffold(
@@ -58,50 +91,80 @@ fun TextInCenter(
     }
 }
 
+fun isHotSpotEnabled(context: Context): Boolean {
+    val wifiManager =
+        context
+            .applicationContext
+            .getSystemService(Context.WIFI_SERVICE) as WifiManager
+    val method = wifiManager.javaClass.getMethod("getWifiApState")
+    method.isAccessible = true
+    /*
+    AP_STATE_DISABLING = 10;
+    AP_STATE_DISABLED = 11;
+    AP_STATE_ENABLING = 12;
+    AP_STATE_ENABLED = 13;
+    AP_STATE_FAILED = 14;
+     */
+    return (method.invoke(wifiManager) as Int) == 13
+}
+
+//TODO говнокод переписать
+fun getWifiApIpAddress(): String? {
+    try {
+        val en = NetworkInterface.getNetworkInterfaces()
+        while (en.hasMoreElements()) {
+            val intf = en.nextElement()
+            if (intf.name.contains("wlan")) {
+                val enumIpAddr = intf.inetAddresses
+                while (enumIpAddr.hasMoreElements()) {
+                    val inetAddress = enumIpAddr.nextElement()
+                    if (!inetAddress.isLoopbackAddress && inetAddress.address.size == 4) {
+                        return inetAddress.hostAddress
+                    }
+                }
+                continue
+            }
+        }
+    } catch (ex: SocketException) {
+        Log.e("ex", ex.toString())
+    }
+    return null
+}
+
 @Preview
 @Composable
 fun MainContent(){
     Column {
-        Spacer(Modifier.size(45.dp))
-        TextInCenter(
-            text = "Включить DevMenu",
-            color = Color.Black
-        )
-        Switch(
-            checked = false,
-            onCheckedChange = {},
-            modifier = Modifier.align(CenterHorizontally)
-        )
-        Spacer(Modifier.size(45.dp))
-        TextInCenter(
-            text = "Порт клиентской части DevMenu",
-            color = Color.Black
-        )
-        PortInput()
-        Spacer(Modifier.size(20.dp))
-        TextButton(
-            modifier = Modifier
-                .clip(RoundedCornerShape(10.dp))
-                .background(MainBlue)
-                .size(80.dp, 30.dp)
-                .align(CenterHorizontally),
-            onClick = { /*TODO*/ }
-        ) {
-            Text(
-                text = "Установить",
-                fontSize = 9.sp,
-                color = Color.White
-            )
-        }
-        Spacer(Modifier.size(30.dp))
 
-        TextInCenter(
-            text = "События",
-            color = Color.Black
+        val context = LocalContext.current
+
+        val text = remember {
+            mutableStateOf("")
+        }
+        var ip = getWifiApIpAddress()
+        if(ip != null)
+            text.value = "IP адрес текущей сети: $ip"
+        else
+            text.value = "Для использования DevMenu подключитесь к WiFi или включите точку доступа"
+
+        thread {
+            Thread.sleep(1000)
+            while(true){
+                ip = getWifiApIpAddress()
+                if(ip != null)
+                    text.value = "IP адрес текущей сети: $ip"
+                else
+                    text.value = "Для использования DevMenu подключитесь к WiFi или включите точку доступа"
+            }
+        }
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = Snapshot.current.enter { text.value }
         )
 
         EventLogField()
-
 
     }
 
